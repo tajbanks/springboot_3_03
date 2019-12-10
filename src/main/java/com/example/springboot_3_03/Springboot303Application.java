dbba8b44ca0ba8f8330d3d6904d98e4e560dfd9f
Allure Anderson <allureanderson@Allures-MacBook-Air.local>
2019-12-10 18:20:09 -0500

    Initial commit

---
+++ src/main/java/com/example/springboot_3_03/Springboot303Application.java
@@ -0,0 +1,12 @@
+package com.example.springboot_3_03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot303Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot303Application.class, args);
+    }
+}
---
+++ src/test/java/com/example/springboot_3_03/Springboot303ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_3_03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot303ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
